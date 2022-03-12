77b1f0726788cdca4dbf0e7c1a77c99505b9388f
DavidQF555 <davidfeng2003@gmail.com>
2022-03-11 18:42:13 -0800

    Added Encoding Option

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { get as http_get, RequestOptions } from 'https';
 
-export async function get(url: RequestOptions | string | URL): Promise<any> {
+export async function get(url: RequestOptions | string | URL, encoding: BufferEncoding): Promise<any> {
 	return new Promise((resolve: Function, reject: Function) => {
 		http_get(url, res => {
-			res.setEncoding('utf-8');
+			res.setEncoding(encoding);
 			let body = '';
 			res.on('data', data => {
 				body += data;
